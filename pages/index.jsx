340a1ba261d59352b318cf7a7f16e3fe7942373c
Ansar <ansar.memon@hotmail.com>
2021-02-16 07:58:12 +0400

    utility(translate): Created and initial version of translate function

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,13 +1,17 @@
 import Head from 'next/head'
+import { useRouter } from 'next/router'
+import translate from '../utils/i18n/translate'
 
 export default function Home() {
+  const { locale } = useRouter();
+
   return (
     <div className="px-28">
       
       <div className="my-24">
         <h1 className="font-medium">Hey Amoury, welcome to the demo</h1>
 
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ipsum nulla, vehicula vitae bibendum sit amet, vehicula ut risus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Praesent at felis magna. Integer euismod lobortis congue. Vivamus eget urna gravida, accumsan ex quis, malesuada est. Donec molestie libero id diam malesuada venenatis. Suspendisse ligula est, mattis ac risus ac, congue consectetur erat. Vestibulum interdum scelerisque suscipit. Fusce ligula augue, hendrerit sit amet pulvinar sit amet, lobortis id leo. Etiam nulla nibh, vulputate nec eros sit amet, rutrum maximus ante. Donec nunc risus, commodo quis lectus in, consequat pulvinar nibh. Donec mattis at nulla in euismod. Vivamus vel arcu arcu.</p>
+        <p>{translate(locale, 'lipsum_dummy_content')}</p>
       </div>
     </div>
   )
---
+++ utils/i18n/translate.js
@@ -0,0 +1,24 @@
+import en from './en.json';
+import ar from './ar.json';
+import fr from './fr.json';
+
+const TRANSLATIONS = {
+  en,
+  ar,
+  fr
+}
+
+const translate = (language = 'en', token) => {
+  let value;
+
+  try {
+    value = TRANSLATIONS[language][token];
+  } catch (error) {
+    console.error('Translation Error ', err);
+    value = token;
+  }
+
+  return value;
+}
+
+export default translate;
